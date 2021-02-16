d65173a2759895536ebb90f06dea6d1168e0470c
Lime5005 <zlp5005@gmail.com>
2021-02-16 23:28:25 +0100

    set const variables for easier maintenance

--- script.js
+++ script.js
@@ -6,6 +6,9 @@ const image2 = document.getElementById('image2')
 const image3 = document.getElementById('image3')
 const textBox = document.getElementById('text-box')
 
+const DARK_MODE = 'dark'
+const LIGHT_MODE = 'light'
+
 function imageMode(color) {
     image1.src = `img/undraw_proud_coder_${color}.svg`
     image2.src = `img/undraw_feeling_proud_${color}.svg`
@@ -13,11 +16,11 @@ function imageMode(color) {
 }
 
 function toggleLightDarkMode(mode) {
-    nav.style.backgroundColor = mode === 'dark' ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)'
-    textBox.style.backgroundColor = mode === 'dark' ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'
-    toggleIcon.children[0].textContent = mode === 'dark' ? 'Dark Mode' : 'Light Mode'
-    mode === 'dark' ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
-    mode === 'dark' ? imageMode('dark') : imageMode('light')
+    nav.style.backgroundColor = mode === DARK_MODE ? 'rgb(0 0 0 / 50%)' : 'rgb(255 255 255 / 50%)'
+    textBox.style.backgroundColor = mode === DARK_MODE ? 'rgb(255 255 255 / 50%)' : 'rgb(0 0 0 / 50%)'
+    toggleIcon.children[0].textContent = mode === DARK_MODE ? 'Dark Mode' : 'Light Mode'
+    mode === DARK_MODE ? toggleIcon.children[1].classList.replace('fa-sun', 'fa-moon') : toggleIcon.children[1].classList.replace('fa-moon', 'fa-sun')
+    mode === DARK_MODE ? imageMode(DARK_MODE) : imageMode(LIGHT_MODE)
 }
 
 function switchMode(event) {
@@ -26,13 +29,13 @@ function switchMode(event) {
     if (event.target.checked) {
         //document.documentElement returns the highest level of the html element, which is usually 'html'
         //setAttribute('key', 'value')
-        document.documentElement.setAttribute('data-theme', 'dark')
-        localStorage.setItem('theme', 'dark')
-        toggleLightDarkMode('dark')
+        document.documentElement.setAttribute('data-theme', DARK_MODE)
+        localStorage.setItem('theme', DARK_MODE)
+        toggleLightDarkMode(DARK_MODE)
     } else {
-        document.documentElement.setAttribute('data-theme', 'light')
-        localStorage.setItem('theme', 'light')
-        toggleLightDarkMode('light')
+        document.documentElement.setAttribute('data-theme', LIGHT_MODE)
+        localStorage.setItem('theme', LIGHT_MODE)
+        toggleLightDarkMode(LIGHT_MODE)
     }
 }
 
@@ -43,8 +46,8 @@ const currentTheme = localStorage.getItem('theme')
     // The first time there is no theme yet until checked:
 if (currentTheme) {
     document.documentElement.setAttribute('data-theme', currentTheme)
-    if (currentTheme === 'dark') {
+    if (currentTheme === DARK_MODE) {
         toggleSwitch.checked = true
-        toggleLightDarkMode('dark')
+        toggleLightDarkMode(DARK_MODE)
     }
 }
